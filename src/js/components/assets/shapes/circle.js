ce3711ebbebfb5e0b303b2fcc04aef25a656d886
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-12-01 00:04:52 +0000

    Fix Webpack file-loader

--- src/js/components/assets/shapes/circle.js
+++ src/js/components/assets/shapes/circle.js
@@ -6,8 +6,10 @@ export default class Logo extends Component {
             <div style={this.props.style}>
                 <div
                     style={{
+                        display: "block",
                         width: "100px",
                         height: "100px",
+                        margin: "0 auto",
                         border: "1px solid #ffffff",
                         borderRadius: "50%",
                         backgroundColor: "#00bb5d"
