fa39db1c823d77164b4ab9734e79d94560b32221
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2018-07-22 12:42:44 +0100

    New site first components

---
+++ src/js/components/skills/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Skills extends Component {
+  render() {
+    return (
+      <div className="me__skills">
+        <div>skills</div>
+      </div>
+    );
+  }
+}
