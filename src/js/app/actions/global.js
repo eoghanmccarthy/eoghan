5f49e1270ef9bf16cc030d66208b5cffbc596ee8
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2018-07-22 23:40:48 +0100

    delete old

--- src/js/app/actions/global.js
+++
@@ -1,7 +0,0 @@
-import { TOGGLE_GLOBAL_HEADER } from "../types/global";
-
-export const toggleGlobalHeader = () => {
-    return {
-        type: TOGGLE_GLOBAL_HEADER
-    };
-};
--- src/js/app/reducers/global/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux"
-import { globalHeader } from "./globalHeader"
-
-export const globalReducer = combineReducers({
-    globalHeader
-})
