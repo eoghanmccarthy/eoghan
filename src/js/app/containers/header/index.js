57095efaa5769b716cfaf30fc574cbeb500ed32b
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2018-10-08 21:53:42 +0100

    remove logo

--- src/js/app/containers/header/index.js
+++ src/js/app/containers/header/index.js
@@ -4,10 +4,6 @@ import Logo from "components/logo";
 
 export default class Header extends Component {
   render() {
-    return (
-      <header className="me__header">
-        <Logo />
-      </header>
-    );
+    return <header className="me__header" />;
   }
 }
--- src/js/app/pages/home.js
+++ src/js/app/pages/home.js
@@ -4,14 +4,6 @@ import Skills from "components/skills";
 
 export default class Home extends Component {
   render() {
-    return (
-      <main className="me__page">
-        <div className="page__title">
-          <h3>
-            <mark>frontend developer, designer</mark>
-          </h3>
-        </div>
-      </main>
-    );
+    return <main className="me__page" />;
   }
 }
