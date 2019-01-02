749242ef2dd0ade545102c8d108daeb679743bfd
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2019-01-02 23:45:30 +0000

    target blank fix

--- src/js/app/components/bio/links/index.tsx
+++ src/js/app/components/bio/links/index.tsx
@@ -4,7 +4,7 @@ import GitHub from "assets/logos/GitHub-Mark-120px-plus.png";
 
 const Links: React.FunctionComponent<{}> = () => (
   <div className={"links"}>
-    <a href={"https://github.com/eoghanmccarthy"} target={"blank"}>
+    <a href={"https://github.com/eoghanmccarthy"} target={"_blank"}>
       <img src={GitHub} />
     </a>
   </div>
