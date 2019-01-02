a01bdf86a4a091a3d204cd419f1efe17066e1eaf
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2019-01-02 21:21:50 +0000

    edit skills more

--- src/js/app/config/skills/index.ts
+++ src/js/app/config/skills/index.ts
@@ -5,12 +5,9 @@ const data = [
   { name: "rx-js", category: "frontend" },
   { name: "react", category: "frontend" },
   { name: "react-native", category: "frontend" },
-  { name: "react-router", category: "frontend" },
-  { name: "react-select", category: "frontend" },
   { name: "react-spring", category: "frontend" },
   { name: "redux", category: "frontend" },
   { name: "redux-observable", category: "frontend" },
-  { name: "redux-persist", category: "frontend" },
   { name: "webpack", category: "frontend" },
   { name: "sass", category: "frontend" }
 ];
