16e13ab5b3af981eebd1ad7a104bd50edaa43100
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2020-02-19 22:23:06 +0000

    remove redux

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
-import configureStore from './configureStore';
+import './styles.scss';
 
 import App from 'app';
 
-import './styles.scss';
-
-const initialState = {};
-
 const history = createHistory();
-
-const { store, persistor } = configureStore(initialState, history);
 
 const MOUNT_NODE = document.getElementById('root');
 
 render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router history={history}>
-        <App />
-      </Router>
-    </PersistGate>
-  </Provider>,
+  <Router history={history}>
+    <App />
+  </Router>,
   MOUNT_NODE
 );
