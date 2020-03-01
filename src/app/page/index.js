0c624cb4593c07cf7e81285cd9d3d8ff6ae45887
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2020-03-01 00:40:03 +0000

    remove custom dialog

--- src/app/page/index.js
+++ src/app/page/index.js
@@ -1,32 +1,23 @@
 import React, { Fragment, useState } from 'react';
-import { Button } from '@eoghanmccarthy/ui';
+import { Button, Dialog } from '@eoghanmccarthy/ui';
 import './styles.css';
 
-import { Dialog } from 'componentLib/dialog';
 import Bio from 'app/features/bio';
 import Logo from 'app/features/logo';
 
 const Home = () => {
   const [showDialog, toggleDialog] = useState(false);
+  const close = () => toggleDialog(false);
+  const toggle = () => toggleDialog(vis => !vis);
 
   return (
     <Fragment>
-      <Dialog
-        id={'bio-dialog'}
-        isVisible={showDialog}
-        closeDialog={() => toggleDialog(!showDialog)}
-        onClick={() => toggleDialog(!showDialog)}
-      >
+      <Dialog id={'bio-dialog'} isVisible={showDialog} closeDialog={close} onClick={close}>
         <Bio />
       </Dialog>
       <main className={'me__page page__index'}>
         <Logo />
-        <Button
-          size={'xl'}
-          shape={'circle'}
-          className={'toggle-bio'}
-          onClick={() => toggleDialog(!showDialog)}
-        >
+        <Button size={'xl'} shape={'circle'} className={'toggle-bio'} onClick={toggle}>
           <span />
           <span />
         </Button>
