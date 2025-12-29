0f8c28457f0972a8bc906bb7d9539bcef3ca596e
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2025-12-29 13:46:30 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,9 @@ function App() {
     <div className="min-h-screen bg-gray-100 p-12 font-serif text-gray-700">
       <div className="max-w-2xl">
         <p className="mb-12 leading-relaxed">
-          eoghan mccarthy is a developer with a design background who builds machine learning tooling.
-          working primarily with javascript, typescript, and python, he approaches technical problems
-          with good instincts for ui/ux and an eye for the seemingly ordinary: the power in subtlety
-          and simplicity.
+          eoghan mccarthy builds tools for machine learning engineers. he works primarily in
+          javascript, typescript, and python creating interfaces and systems that prioritize
+          clarity and usability, informed by a background in design.
         </p>
 
         <div className="mt-16 space-y-6">
@@ -27,7 +26,7 @@ function App() {
           </div>
 
           <div>
-            <div className="text-xs tracking-widest uppercase mb-2 font-sans">work</div>
+            <div className="text-xs tracking-widest uppercase mb-2 font-sans">links</div>
             <div>
               <a
                 href="https://github.com/eoghanmccarthy"
@@ -40,10 +39,6 @@ function App() {
             </div>
           </div>
         </div>
-
-        {/*<footer className="fixed bottom-12 left-12 right-12 flex justify-between text-xs">*/}
-        {/*  <div>eoghan mccarthy © all rights reserved</div>*/}
-        {/*</footer>*/}
       </div>
     </div>
   )
